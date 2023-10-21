f11d5cbc987ded3c113e7ec02224c1b9ebf23ddc
DmytroLamashevskyi <d.lamashevskyi@gmail.com>
2023-10-21 03:42:28 +0200

    Chages

--- OpenAIProject/reactapp/src/App.jsx
+++ OpenAIProject/reactapp/src/App.jsx
@@ -4,15 +4,14 @@ export default class App extends Component {
     static displayName = App.name;
 
     constructor(props) {
-        super(props);
-        this.state = { forecasts: [], loading: true };
+        super(props); 
     }
 
     componentDidMount() {
-        this.populateWeatherData();
+
     }
 
-    static renderForecastsTable(forecasts) {
+    static renderAiMessage(forecasts) {
         return (
             <table className='table table-striped' aria-labelledby="tabelLabel">
                 <thead>
@@ -37,23 +36,14 @@ export default class App extends Component {
         );
     }
 
-    render() {
-        let contents = this.state.loading
-            ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-            : App.renderForecastsTable(this.state.forecasts);
+    render() { 
 
         return (
             <div>
                 <h1 id="tabelLabel" >Weather forecast</h1>
-                <p>This component demonstrates fetching data from the server.</p>
-                {contents}
+                <p>This component demonstrates fetching data from the server.</p> 
             </div>
         );
     }
-
-    async populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        this.setState({ forecasts: data, loading: false });
-    }
+     
 }
